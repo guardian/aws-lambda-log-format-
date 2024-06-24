e029a99e7556a94b5bffb043ded6c2a0762d57a4
akash1810 <akash1810@users.noreply.github.com>
2024-06-24 20:47:59 +0100

    Initial commit

---
+++ test/aws-lambda-log-format.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaLogFormat from '../lib/aws-lambda-log-format-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lambda-log-format-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaLogFormat.AwsLambdaLogFormatStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
