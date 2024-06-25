ebe8f1589d5790a9d183deb482c20dac25c3e68e
akash1810 <akash1810@users.noreply.github.com>
2024-06-25 10:15:05 +0100

    Distinguish log lines from each other

--- src/lambda.ts
+++ src/lambda.ts
@@ -17,23 +17,27 @@ function getConfig() {
 export async function main(...args: unknown[]) {
  const { stage, app } = getConfig();
 
+ console.info(`${app} (${stage}) begin`);
+
  console.debug(
   `${app} (${stage}) called with ${args.map((arg) => JSON.stringify(arg)).join(", ")}`,
  );
 
  // This log should appear in the `message` field within Central ELK.
- console.log("This is a plain text message");
+ console.log("[log 1] This is a plain text message");
 
  // This log should appear in the `message` field within Central ELK, with a `status` marker.
- console.log({ message: "This is a JSON message with markers", status: 200 });
+ console.log({ message: "[log 2] This is a JSON message with markers", status: 200 });
 
  // This log should appear in the `message` field within Central ELK, with a `status` marker.
- console.log(JSON.stringify({ message: "This is a JSON.stringify message with markers", status: 200 }));
+ console.log(JSON.stringify({ message: "[log 3] This is a JSON.stringify message with markers", status: 200 }));
 
  // This log should appear in the `message` field within Central ELK, with a `status` marker.
  // It probably will not, as that's not how `console.log` works!
- console.log("This is a plain text message with markers", { status: 200 });
+ console.log("[log 4] This is a plain text message with markers", { status: 200 });
 
  // This log should appear in the `message` field within Central ELK, with a `status` marker.
- console.log(`This is a plain text message with markers ${JSON.stringify('%s')}`, { status: 200 });
+ console.log(`[log 5] This is a plain text message with markers ${JSON.stringify('%s')}`, { status: 200 });
+
+ console.info(`${app} (${stage}) end`);
 }
